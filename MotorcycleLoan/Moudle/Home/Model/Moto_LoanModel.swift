f1b7ccc19af3d12cb46743ab36cd46cde3d0142a
Relyn Acha <relynacha@Relyns-MacBook-Air.local>
2024-06-14 14:11:44 +0800

    bug fix

--- MotorcycleLoan/Moudle/Home/Model/Moto_LoanModel.swift
+++ MotorcycleLoan/Moudle/Home/Model/Moto_LoanModel.swift
@@ -25,8 +25,20 @@ struct Moto_LoadTermModel: Codable {
     var status: Int?
     init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.stage_number = try container.decodeIfPresent(String.self, forKey: .stage_number)
-        self.days_number = try container.decodeIfPresent(String.self, forKey: .days_number)
+        if let _stage_number = try? container.decodeIfPresent(String.self, forKey: .stage_number) {
+            stage_number = _stage_number
+        }else {
+            let _stage_number = try container.decodeIfPresent(Int.self, forKey: .stage_number)
+            stage_number = "\(_stage_number ?? 0)"
+        }
+        
+        if let _days_number = try? container.decodeIfPresent(String.self, forKey: .days_number) {
+            days_number = _days_number
+        }else {
+            let _days_number = try container.decodeIfPresent(Int.self, forKey: .days_number)
+            days_number = "\(_days_number ?? 0)"
+        }
+        
         if let _status = try? container.decodeIfPresent(Int.self, forKey: .status) {
             status = _status
         }else {
@@ -42,8 +54,19 @@ struct Moto_UserDataModel: Codable {
     var info: Moto_LoadUserDataInfoModel?
     init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.days = try container.decodeIfPresent(String.self, forKey: .days)
-        self.term_id = try container.decodeIfPresent(String.self, forKey: .term_id)
+        if let _term_id = try? container.decodeIfPresent(String.self, forKey: .term_id) {
+            term_id = _term_id
+        }else {
+            let _term_id = try container.decodeIfPresent(Int.self, forKey: .term_id)
+            term_id = "\(_term_id ?? 0)"
+        }
+        
+        if let _days = try? container.decodeIfPresent(String.self, forKey: .days) {
+            days = _days
+        }else {
+            let _days = try container.decodeIfPresent(Int.self, forKey: .days)
+            days = "\(_days ?? 0)"
+        }
         self.info = try container.decodeIfPresent(Moto_LoadUserDataInfoModel.self, forKey: .info)
     }
 }
@@ -55,7 +78,13 @@ struct Moto_LoadUserDataInfoModel: Codable {
     var data: [Moto_LoadUserDataInfoDataModel]?
     init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.pro_id = try container.decodeIfPresent(String.self, forKey: .pro_id)
+        if let _pro_id = try? container.decodeIfPresent(String.self, forKey: .pro_id) {
+            pro_id = _pro_id
+        }else {
+            let _pro_id = try container.decodeIfPresent(Int.self, forKey: .pro_id)
+            pro_id = "\(_pro_id ?? 0)"
+        }
+        
         if let _money = try? container.decodeIfPresent(String.self, forKey: .money) {
             money = _money
         }else {
@@ -79,18 +108,60 @@ struct Moto_LoadUserDataInfoDataModel: Codable {
     var repay_price: Int?
     init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.rid = try container.decodeIfPresent(String.self, forKey: .rid)
-        self.service_fee = try container.decodeIfPresent(Int.self, forKey: .service_fee)
-        self.credit_price = try container.decodeIfPresent(Int.self, forKey: .credit_price)
-        self.manage_price = try container.decodeIfPresent(Int.self, forKey: .manage_price)
-        self.interest = try container.decodeIfPresent(Int.self, forKey: .interest)
-        self.real_price = try container.decodeIfPresent(Int.self, forKey: .real_price)
-        self.repay_price = try container.decodeIfPresent(Int.self, forKey: .repay_price)
+        if let _rid = try? container.decodeIfPresent(String.self, forKey: .rid) {
+            rid = _rid
+        }else {
+            let _rid = try container.decodeIfPresent(Int.self, forKey: .rid)
+            rid = "\(_rid ?? 0)"
+        }
+        
         if let _amount = try? container.decodeIfPresent(Int.self, forKey: .amount) {
             amount = _amount
         }else {
             let _amount = try container.decodeIfPresent(String.self, forKey: .amount)
             amount = Int(_amount ?? "")
+        }
+        
+        if let _service_fee = try? container.decodeIfPresent(Int.self, forKey: .service_fee) {
+            service_fee = _service_fee
+        }else {
+            let _service_fee = try container.decodeIfPresent(String.self, forKey: .service_fee)
+            service_fee = Int(_service_fee ?? "")
+        }
+        
+        if let _credit_price = try? container.decodeIfPresent(Int.self, forKey: .credit_price) {
+            credit_price = _credit_price
+        }else {
+            let _credit_price = try container.decodeIfPresent(String.self, forKey: .credit_price)
+            credit_price = Int(_credit_price ?? "")
+        }
+        
+        if let _manage_price = try? container.decodeIfPresent(Int.self, forKey: .manage_price) {
+            manage_price = _manage_price
+        }else {
+            let _manage_price = try container.decodeIfPresent(String.self, forKey: .manage_price)
+            manage_price = Int(_manage_price ?? "")
+        }
+        
+        if let _interest = try? container.decodeIfPresent(Int.self, forKey: .interest) {
+            interest = _interest
+        }else {
+            let _interest = try container.decodeIfPresent(String.self, forKey: .interest)
+            interest = Int(_interest ?? "")
+        }
+        
+        if let _real_price = try? container.decodeIfPresent(Int.self, forKey: .real_price) {
+            real_price = _real_price
+        }else {
+            let _real_price = try container.decodeIfPresent(String.self, forKey: .real_price)
+            real_price = Int(_real_price ?? "")
+        }
+        
+        if let _repay_price = try? container.decodeIfPresent(Int.self, forKey: .repay_price) {
+            repay_price = _repay_price
+        }else {
+            let _repay_price = try container.decodeIfPresent(String.self, forKey: .repay_price)
+            repay_price = Int(_repay_price ?? "")
         }
         
         if let _tech_price = try? container.decodeIfPresent(Int.self, forKey: .tech_price) {
