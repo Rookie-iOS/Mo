b2b93d15da04b3900f481420c95d13143073e0b0
Yanice <1334964457@qq.com>
2024-06-18 17:40:29 +0800

    创建风控模型类

---
+++ MotorcycleLoan/Service/Moto_EventRisk.swift
@@ -0,0 +1,146 @@
+//
+//  Moto_EventRisk.swift
+//  MotorcycleLoan
+//
+//  Created by Yanice on 2024/6/18.
+//
+
+import Foundation
+
+class Moto_EventRisk {
+    
+    var client_id = -999
+    var apply_now_time = -999
+    var basic_confirm_time = -999
+    var basic_confirm_duration = -999
+    var purpose_last_duration = -999
+    var purpose_updatecount = -999
+    var purpose_all_duration = "-999"
+    var email_duration = -999
+    var emai_updatecount = -999
+    var facebook_name_duration = -999
+    var facebook_name_updatecount = -999
+    var messenger_viber_duration = -999
+    var messenger_viber_updatecount = -999
+    var backup_phone_duration = -999
+    var backup_phone_updatecount = -999
+    var Industry_category_duration = -999
+    var Industry_category_updatecount = -999
+    // 1:  2:
+    var Industry_category_add_type = 1
+    var religion_duration = -999
+    var religion_updatecount = -999
+    var marry_status_duration = -999
+    var marry_status_updatecount = -999
+    var kids_num_duration = -999
+    var kids_num_update_count = -999
+    var education_duration = -999
+    var education_updatecount = -999
+    var address_completed_duration = -999
+    var residence_duration = -999
+    var residence_updatecount = -999
+    var detail_address_duration = -999
+    var detail_address_updatetime = -999
+    var job_information_duration = -999
+    var job_information_updatecount = -999
+    var company_name_duration = -999
+    var company_name_updatecount = -999
+    var working_Experienct_duration = -999
+    var working_Experienct_updatecount = -999
+    var payday_first_duration = -999
+    var payday_first_updatecount = -999
+    var payday_second_duration = -999
+    var payday_second_updatecount = -999
+    var month_income_duration = -999
+    var month_income_updatecount = -999
+    var company_address_duration = -999
+    var company_address_updatecount = -999
+    var company_detail_address_duration = -999
+    var company_detail_address_updatecount = -999
+    var emergency_contact_click_time = -999
+    var emergency_contact_format_errorcount = -999
+    var emergency_contact_duplicate_errorcount = -999
+    var emergency_fill_completed_duration = -999
+    var emergency_fill_one_duration = -999
+    var emergency_fill_name_one_duration = -999
+    var emergency_fill_name_one_updatecount = -999
+    var emergency_fill_one_phone_duration = -999
+    var emergency_fill_one_phone_updatecount = -999
+    var emergency_contact_one_relation_duration = -999
+    var emergency_contact_one_relation_updatecount = -999
+    var emergency_fill_two_duration = -999
+    var emergency_fill_name_two_duration = -999
+    var emergency_fill_name_two_updatecount = -999
+    var emergency_fill_two_phone_duration = -999
+    var emergency_fill_two_phone_updatecount = -999
+    var emergency_contact_two_relation_duration = -999
+    var emergency_contact_two_relation_updatecount = -999
+    var emergency_fill_three_duration = -999
+    var emergency_fill_name_three_duration = -999
+    var emergency_fill_name_three_updatecount = -999
+    var emergency_fill_three_phone_duration = -999
+    var emergency_fill_three_phone_updatecount = -999
+    var emergency_contact_three_relation_duration = -999
+    var emergency_contact_three_relation_updatecount = -999
+    var emergency_fill_four_duration = -999
+    var emergency_fill_name_four_duration = -999
+    var emergency_fill_name_four_updatecount = -999
+    var emergency_fill_four_phone_duration = -999
+    var emergency_fill_four_phone_updatecount = -999
+    var emergency_contact_four_relation_duration = -999
+    var emergency_contact_four_relation_updatecount = -999
+    var emergency_fill_five_duration = -999
+    var emergency_fill_name_five_duration = -999
+    var emergency_fill_name_five_updatecount = -999
+    var emergency_fill_five_phone_duration = -999
+    var emergency_fill_five_phone_updatecount = -999
+    var emergency_contact_five_relation_duration = -999
+    var emergency_contact_five_relation_updatecount = -999
+    var backend_num = -999
+    
+    // cer
+    var idcard_click_time = -999
+    var idcard_type_duration = -999
+    var idcard_type_updatecount = -999
+    var idcard_photo_document_duration = -999
+    var idcard_photo_document_updatecount = -999
+    var idcard_size = -999
+    var idcard_cps_size = -999
+    var face_photo_document_duration = -999
+    var face_photo_document_requestcount = -999
+    var face_photo_document_errorcount = -999
+    var face_size = -999
+    var idcard_number_duration = -999
+    var idcard_number_updatecount = -999
+    var idcard_is_echo = -999
+    var idcard_echocount = -999
+    var name_one_duration = -999
+    var name_one_updatecount = -999
+    var name_two_duration = -999
+    var name_two_updatecount = -999
+    var name_three_duration = -999
+    var name_three_updatecount = -999
+    var gender_updatecount = -999
+    var birthday_duration = -999
+    var birthday_updatecounter = -999
+    var backend_num_id = -999
+    var idcard_submit_time = -999
+    var page_duration = -999
+    
+    // extra
+    var upload_attachment = -999
+    var employee_card_payslip_duration = -999
+    var employee_card_payslip_updatecount = -999
+    var other_work_photo_duration = -999
+    var other_work_photo_updatecount = -999
+    var business_permit_duration = -999
+    var business_permit_updatecount = -999
+    var other_business_duration = -999
+    var other_business_updatecount = -999
+    var repayment_vouchers_duraion = -999
+    var repayment_vouchers_updatecount = -999
+    var add_more_duration = -999
+    var add_more_updatecount = -999
+    var next_time = -999
+    var page_duration_option = -999
+}
